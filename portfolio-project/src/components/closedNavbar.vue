13131ccfdad1328a646b214182639a57675417b6
Thomas-Anderson2024 <tanderson0876@gmail.com>
2022-06-09 17:10:33 -0400

    media query and favicon

--- portfolio-project/src/components/closedNavbar.vue
+++ portfolio-project/src/components/closedNavbar.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="closedNavbar" >
-        <img class="logo" src="/android-chrome-512x512.png" @click="menutoggle()">
-        <div class="menu" v-if="menuopen == true">
-<router-link to="/" class="pageSelector subheader" value="Home" >Home</router-link><br>
-<router-link to="education" class="pageSelector subheader" value="Education" >Education</router-link><br>
-<router-link to="skills" class="pageSelector subheader" value="Skills" >Skills</router-link><br>
-<router-link to="sports" class="pageSelector subheader" value="Sports" >Sports</router-link><br>
-<router-link to="work" class="pageSelector subheader" value="Work" >Work</router-link><br>
+        <div class="alwaysonscreenholder" @click="menutoggle()">
+        <img class="logo" src="/android-chrome-512x512.png" >
+        <h4 class="subtleHint" v-if="menuopen == false">Click the Logo To Explore More</h4>
+        </div>
+        <div class="menu" v-if="menuopen == true" >
+<router-link  to="/" class="pageSelector subheader" value="Home" @click="menutoggle()">Home</router-link><br>
+<router-link to="education" class="pageSelector subheader" value="Education" @click="menutoggle()">Education</router-link><br>
+<router-link to="skills" class="pageSelector subheader" value="Skills" @click="menutoggle()">Skills</router-link><br>
+<router-link to="sports" class="pageSelector subheader" value="Sports" @click="menutoggle()">Sports</router-link><br>
+<router-link to="work" class="pageSelector subheader" value="Work" @click="menutoggle()">Work</router-link><br>
 </div>
     </div>
 </template>
@@ -21,5 +24,6 @@ export default {
     menutoggle() {
     this.menuopen = !this.menuopen
 }
-}}
+},
+}
 </script>
