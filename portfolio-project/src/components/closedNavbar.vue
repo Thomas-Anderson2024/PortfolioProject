abc24d33c037e4e41c6b3ae9ed604ca4545f44ef
Thomas-Anderson2024 <tanderson0876@gmail.com>
2022-06-09 16:32:37 -0400

    media query and favicon

---
+++ portfolio-project/src/components/closedNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="closedNavbar" >
+        <img class="logo" src="/android-chrome-512x512.png" @click="menutoggle()">
+        <div class="menu" v-if="menuopen == true">
+<router-link to="/" class="pageSelector subheader" value="Home" >Home</router-link><br>
+<router-link to="education" class="pageSelector subheader" value="Education" >Education</router-link><br>
+<router-link to="skills" class="pageSelector subheader" value="Skills" >Skills</router-link><br>
+<router-link to="sports" class="pageSelector subheader" value="Sports" >Sports</router-link><br>
+<router-link to="work" class="pageSelector subheader" value="Work" >Work</router-link><br>
+</div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menuopen: false,
+    }
+  },
+  methods: {
+    menutoggle() {
+    this.menuopen = !this.menuopen
+}
+}}
+</script>
