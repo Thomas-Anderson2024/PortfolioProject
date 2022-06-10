47453eedcd4fd54ca76c0fa732ff0f59f46e14f7
Thomas-Anderson2024 <90661563+Thomas-Anderson2024@users.noreply.github.com>
2022-06-10 10:58:07 -0400

    fix this pls

--- portfolio-project/src/views/Home.vue
+++ portfolio-project/src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
     <main class="home content translateable">
-        <h1 class="contentText">Hello, My Name Is </h1><br><h1>Thomas Anderson</h1>
+        <h1 class="contentText" v-html="paper">Hello, My Name Is <br> Thomas Anderson</h1>
         <h3 class="contentText">I am Staten Island Technical Highschool <br>student and prospective architect</h3>
     </main>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            titleText:"Thomas Anderson",
+            paper:'',
+            timer:null
+        }
+    },
+    mounted(){
+        this.printer()
+    },
+    methods:{
+        printer(){
+            this,stopper()
+            let i = -1
+             this.timer = setInterval(()=>{
+                 i++
+                 const x = i%this.titleText.length
+                 this
+             })
+             //https://miro.medium.com/max/1400/1*Mo_SVwjgG0d0GoQSkMQl0w.png
+        }
+    }
+}
+</script>
 <style scoped>
 .content{
     align-items: center;
-}
+} 
 </style>
