ccf3dd76d0bb01e1323c05525354209430afea08
William Anderson <48596907+William-Anderson2020@users.noreply.github.com>
2022-06-12 02:59:21 -0400

    type text

--- portfolio-project/src/views/Home.vue
+++ portfolio-project/src/views/Home.vue
@@ -1,37 +1,92 @@
 <template>
     <main class="home content translateable">
-        <h1 class="contentText" v-html="paper">Hello, My Name Is <br> Thomas Anderson</h1>
+        <h1 class="contentText">Hello, My 
+        <span class="typed-text">{{ typeValue2 }}</span>
+         Is<br>
+        <span class="typed-text">{{ typeValue }}</span>
+        <span class="cursor" :class="{'typing': typeStatus}">&nbsp;</span>
+        </h1>
         <h3 class="contentText">I am Staten Island Technical Highschool <br>student and prospective architect</h3>
     </main>
 </template>
 <script>
-export default {
-    data(){
-        return{
-            titleText:"Thomas Anderson",
-            paper:'',
-            timer:null
+    export default{
+        data: () =>{
+          return {
+              typeValue: '',
+              typeValue2: '',
+              typeStatus: false,
+              typeArray: ['Thomas Anderson', 'Tanderson0876@gmail.com'],
+              typeArray2: ['Name', 'Email'],
+              typingSpeed: 150,
+              erasingSpeed: 100,
+              newTextDelay: 2000,
+              textArrayIndex: 0,
+              charIndex: 0,
+          }
+        },
+        methods:{
+            typeText(){
+                if(this.charIndex < this.typeArray[this.textArrayIndex].length){
+                    if(!this.typeStatus)
+                    this.typeStatus = true;
+
+                    this.typeValue += this.typeArray[this.textArrayIndex].charAt(this.charIndex); 
+                    this.typeValue2 += this.typeArray2[this.textArrayIndex].charAt(this.charIndex);
+                    this.charIndex += 1;
+
+                    setTimeout(this.typeText, this.typingSpeed)
+                }
+                else{
+                    this.typeStatus = false;
+                    setTimeout(this.eraseText, this.newTextDelay);
+                }
+            },
+            eraseText(){
+                if(this.charIndex > 0){
+                    if(!this.typeStatus)
+                    this.typeStatus = true;
+
+                    this.typeValue = this.typeArray[this.textArrayIndex].substring(0, this.charIndex - 1);
+                    this.typeValue2 = this.typeArray2[this.textArrayIndex].substring(0, this.charIndex - 1);
+                    this.charIndex -= 1;
+                    setTimeout(this.eraseText, this.erasingSpeed);
+                }
+                else {
+                    this.typeStatus = false;
+                    this.textArrayIndex += 1;
+                    if(this.textArrayIndex >= this.typeArray.length)
+                    this.textArrayIndex = 0;
+
+                    setTimeout(this.typeText, this.typingSpeed - 1000);
+                }
+            },
+        },
+        created(){
+            setTimeout(this.typeText, this.newTextDelay + 200)
         }
-    },
-    mounted(){
-        this.printer()
-    },
-    methods:{
-        printer(){
-            this,stopper()
-            let i = -1
-             this.timer = setInterval(()=>{
-                 i++
-                 const x = i%this.titleText.length
-                 this
-             })
-             //https://miro.medium.com/max/1400/1*Mo_SVwjgG0d0GoQSkMQl0w.png
         }
-    }
-}
 </script>
 <style scoped>
 .content{
     align-items: center;
-} 
+}
+.typed-text{
+    font-size: var(--h1);
+}
+.cursor{
+    display: inline-block;
+    margin-left: 1rem;
+    background-color: black;
+    animation: cursorBlink 1s infinite;
+    width: .25rem;
+}
+.cursor.typing{
+    animation: none;
+}
+@keyframes cursorBlink {
+    49%{ background-color: black; }
+    50%{ background-color: transparent; }
+    99%{ background-color: black; }
+}
 </style>
