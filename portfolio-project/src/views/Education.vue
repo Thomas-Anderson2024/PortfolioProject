8fe7358c04592b86ef07979385b877d72259deea
Thomas-Anderson2024 <90661563+Thomas-Anderson2024@users.noreply.github.com>
2023-05-30 12:30:17 -0400

    work

--- portfolio-project/src/views/Education.vue
+++ portfolio-project/src/views/Education.vue
@@ -6,16 +6,6 @@
        <h3>-</h3>  <img class="schoolImage" src="https://operoo.com/wp-content/uploads/2020/08/SITHSseal_MED_CLR_WHT-1-600x600-1.png"><h3> Staten Island Technical High School</h3>
     </div>
     <h4 class="gradDate">- Days Until Graduation: {{Final_Result}}</h4>
-    <h2 class="header">Prospective Schools:</h2>
-    <div class="listItem">
-       <h3>-</h3>  <img class="schoolImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Cornell_University_seal.svg/1200px-Cornell_University_seal.svg.png"><h3> Cornell University</h3>
-    </div>
-    <div class="listItem">
-       <h3>-</h3>  <img class="schoolImage" src="https://upload.wikimedia.org/wikipedia/commons/d/d0/Princeton_seal.svg"><h3> Princeton University</h3>
-    </div>
-    <div class="listItem">
-       <h3>-</h3>  <img class="schoolImage" src="https://brand.rice.edu/sites/g/files/bxs2591/files/2019-08/190308_Rice_Mechanical_Brand_Standards_Logos-2.png"><h3> Rice University</h3>
-    </div>
     </main>
 </template>
 <script setup>
--- portfolio-project/src/views/Work.vue
+++ portfolio-project/src/views/Work.vue
@@ -8,11 +8,11 @@
                 <li> Student and staff network configuration.</li>
                 <li> Inventory management and storage organization.</li>
             </ul>
-        <h2>Volunteer Experience:</h2>
-            <h3 class="header">I.S.27 IT Team</h3>
+            <h3 class="header">AIA Discover Architecture Internship: SpaceSmith Architecture Firm</h3>
             <ul class="skillList">
-                <li>Found measures to cut down on the school electricity budget.</li>
-                <li> Provided technical support to teachers and peers during lunch hours.</li>
+                <li>Developed interior design skills.</li>
+                <li> Produced hand-drawn and CAD modeled designs.</li>
+                <li> </li>
             </ul>
     </main>
 </template>
